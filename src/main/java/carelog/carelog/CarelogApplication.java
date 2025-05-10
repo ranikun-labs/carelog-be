91f7781e068501531fc14ac3e96d9ea8f829ea1c
aixion1506 <aixion1506@gmail.com>
2025-05-10 18:51:20 +0900

    Chore: carelog Init

---
+++ src/main/java/carelog/carelog/CarelogApplication.java
@@ -0,0 +1,13 @@
+package carelog.carelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carelog/carelog/CarelogApplicationTests.java
@@ -0,0 +1,13 @@
+package carelog.carelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
