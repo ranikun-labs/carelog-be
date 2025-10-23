99e5604c3103313b8d9104bcdb6d68cac12c5904
aixion1506 <aixion1506@gmail.com>
2025-10-23 13:11:09 +0900

    ✨ feat: Swagger 추가

---
+++ src/main/java/carelog/carelog/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+
+package carelog.carelog.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("Carelog API")
+                .description("Carelog 서비스의 API 명세서입니다.");
+
+        String securitySchemeName = "bearerAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
