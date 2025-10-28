d14e8c936758512ff5b51800589858f0d796a342
aixion1506 <aixion1506@gmail.com>
2025-10-29 00:43:21 +0900

    ✨ feat: RelationRepository에 관계 조회 메서드 추가

--- src/main/java/carelog/carelog/relation/domain/RelationRepository.java
+++ src/main/java/carelog/carelog/relation/domain/RelationRepository.java
@@ -1,13 +1,19 @@
 package carelog.carelog.relation.domain;
 
-import carelog.carelog.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import carelog.carelog.user.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public interface RelationRepository extends JpaRepository<Relation, Long> {
 
     Optional<Relation> findByManagerAndCustomer(User manager, User customer);
+
+    List<Relation> findAllManger(User user);
+
+    List<Relation> findAllByCustomer(User customer);
+
+    boolean existsByManagerAndCustomer(User manager, User customer);
 }
