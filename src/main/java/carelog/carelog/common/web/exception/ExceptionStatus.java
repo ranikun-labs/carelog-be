58925119975de43b3f31e879dda00692029cfad7
aixion1506 <aixion1506@gmail.com>
2025-10-29 00:41:27 +0900

    ✨ feat: ExceptionStatus에 INVALID_USER_ROLE 추가

--- src/main/java/carelog/carelog/common/web/exception/ExceptionStatus.java
+++ src/main/java/carelog/carelog/common/web/exception/ExceptionStatus.java
@@ -1,8 +1,7 @@
 package carelog.carelog.common.web.exception;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.*;
+import org.springframework.http.*;
 
 @Getter
 @RequiredArgsConstructor
@@ -13,7 +12,10 @@ public enum ExceptionStatus {
 
     // 409 CONFLICT
     DUPLICATE_USER_ID(HttpStatus.CONFLICT, "이미 존재하는 사용자 ID입니다."),
-    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+
+    // 400 BAD_REQUEST
+    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 역활입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
