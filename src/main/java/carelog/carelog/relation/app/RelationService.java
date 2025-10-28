33d0c6b811642ac8fa33a24231a70acbe5966679
aixion1506 <aixion1506@gmail.com>
2025-10-28 18:00:06 +0900

    ✨ feat: RelationService 인터페이스 추가

---
+++ src/main/java/carelog/carelog/relation/app/RelationService.java
@@ -0,0 +1,25 @@
+package carelog.carelog.relation.app;
+
+import carelog.carelog.relation.domain.Relation;
+import carelog.carelog.relation.domain.RelationStatus;
+import carelog.carelog.user.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RelationService {
+
+    Relation createRelation(User manger, User customer);
+
+    Optional<Relation> findRelationById(Long relationId);
+
+    Optional<Relation> findRelationByManagerAndCustomer(User manager, User customer);
+
+    List<Relation> findAllRelationsByManager(User manager);
+
+    List<Relation> findAllRelationsByCustomer(User customer);
+
+    Relation updateRelationsStatus(Long relationId, RelationStatus newStatus);
+
+    void deleteRelation(Long relationId);
+}
