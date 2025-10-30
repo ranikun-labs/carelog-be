defc43e0d497b673aacf76c26d1e276f80202ea4
aixion1506 <aixion1506@gmail.com>
2025-10-30 14:16:45 +0900

    ✨ feat: RELATION_ALREADY_EXISTS 예외상태 추가

--- src/main/java/carelog/carelog/common/web/exception/ExceptionStatus.java
+++ src/main/java/carelog/carelog/common/web/exception/ExceptionStatus.java
@@ -7,6 +7,9 @@ import org.springframework.http.*;
 @RequiredArgsConstructor
 public enum ExceptionStatus {
 
+    // 400 BAD_REQUEST
+    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 역활입니다."),
+
     // 404 NOT_FOUND
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     RELATION_NOT_FOUND(HttpStatus.NOT_FOUND, "관계를 찾을 수 없습니다."),
@@ -14,9 +17,7 @@ public enum ExceptionStatus {
     // 409 CONFLICT
     DUPLICATE_USER_ID(HttpStatus.CONFLICT, "이미 존재하는 사용자 ID입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-
-    // 400 BAD_REQUEST
-    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 역활입니다.");
+    RELATION_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 관계입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
