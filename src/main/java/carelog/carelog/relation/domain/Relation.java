f627b68cee75344b150f9d6bece1ff4075ece12d
aixion1506 <aixion1506@gmail.com>
2025-11-03 23:29:43 +0900

    ✨ feat: 관계 중복 생성을 방지하기 위한 동시성 제어 추가

--- src/main/java/carelog/carelog/relation/domain/Relation.java
+++ src/main/java/carelog/carelog/relation/domain/Relation.java
@@ -11,7 +11,12 @@ import org.hibernate.annotations.*;
 @Entity
 @SQLDelete(sql = "UPDATE relations SET deleted_at = NOW() WHERE id = ?")
 @SQLRestriction("deleted_at IS NULL")
-@Table(name = "relations")
+@Table(name = "relations", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_manager_customer",
+                columnNames = {"manager_id", "customer_id"}
+        )
+})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Relation extends BaseEntity {
