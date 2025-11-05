32568f5a9044f11d55796d3ff38c20bb470ad7f8
aixion1506 <aixion1506@gmail.com>
2025-11-05 17:18:15 +0900

    ✨ feat: Relation DTO에서 @NotNull 유효성 검사 추가

--- src/main/java/carelog/carelog/relation/web/dto/RelationCreateRequest.java
+++ src/main/java/carelog/carelog/relation/web/dto/RelationCreateRequest.java
@@ -1,11 +1,16 @@
 package carelog.carelog.relation.web.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class RelationCreateRequest {
+
+    @NotNull(message = "매니저ID 는 필수 입력 값입니다.")
     private Long managerId;
+
+    @NotNull(message = "고객ID 는 필수 입력 값입니다.")
     private Long customerId;
 }
