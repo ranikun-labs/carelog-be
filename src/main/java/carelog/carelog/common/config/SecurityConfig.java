862cc2b9c34ecdf7db76800e76a1601950283124
aixion1506 <aixion1506@gmail.com>
2025-11-14 17:13:22 +0900

    ♻️ refactor: 개발중 API 테스트에 대한 인가 해제

--- src/main/java/carelog/carelog/common/config/SecurityConfig.java
+++ src/main/java/carelog/carelog/common/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.*;
 import org.springframework.security.web.*;
-import org.springframework.security.web.util.matcher.*;
 
 @Configuration
 @EnableWebSecurity
@@ -20,21 +19,23 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         // 운영용이라 차후에 활성화
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/"),
-                                new AntPathRequestMatcher("/api/v1"),
-                                new AntPathRequestMatcher("/api/v1/"),
-                                new AntPathRequestMatcher("/login"),
-                                new AntPathRequestMatcher("/api/auth/signup"),
-                                new AntPathRequestMatcher("/users"),
-                                new AntPathRequestMatcher("/api/health"),
-                                new AntPathRequestMatcher("/error**"),
-                                new AntPathRequestMatcher("/api/v1/error**")
-                        ).permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll() // API 문서 허용
-                        .anyRequest().authenticated());
-
+//                        .requestMatchers(
+//                                new AntPathRequestMatcher("/"),
+//                                new AntPathRequestMatcher("/api/v1"),
+//                                new AntPathRequestMatcher("/api/v1/"),
+//                                new AntPathRequestMatcher("/login"),
+//                                new AntPathRequestMatcher("/api/auth/signup"),
+//                                new AntPathRequestMatcher("/users"),
+//                                new AntPathRequestMatcher("/api/health"),
+//                                new AntPathRequestMatcher("/error**"),
+//                                new AntPathRequestMatcher("/api/v1/error**")
+//                        ).permitAll()
+//                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+//                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll() // API 문서 허용
+//                        ---- 아래 authenticated가 보안 로직 ----
+//                        .anyRequest().authenticated());
+                        // 개발중: 모든 요청 허용
+                        .anyRequest().permitAll());
         return http.build();
     }
 
